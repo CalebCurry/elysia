ae88c44215f3add7f1dfa0e1226138f36bdaf7ec
Caleb Curry <caleb@calebcurry.com>
2026-09-06 00:05:22 -0400

    first example route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Caleb!").listen(3000);
+const campaigns = [
+  {
+    campaignId: 1,
+    name: "Summer Sale",
+    dueDate: new Date(),
+    createdAt: new Date()
+  },
+  {
+    campaignId: 2,
+    name: "Black Friday",
+    dueDate: new Date(),
+    createdAt: new Date()
+  }
+]
+
+const app = new Elysia({"prefix": "/api/v1"})
+  .get("/campaigns", () => {
+    return {data: campaigns}
+  })
+  .get("/", () => "Hello Caleb!")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
